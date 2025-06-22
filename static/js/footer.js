625acf012491fefee0ea1382aa005e025b9b9f4d
mifizz <mfx.mail.g@gmail.com>
2025-06-22 22:25:03 +0200

    Update css and some pages including main page

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,5 +1,9 @@
-document.getElementById('footer').innerHTML = `
+document.getElementById("footer").innerHTML = `
     <footer>
-        <a href="/">mifizz.pw</a>
+        <a href="/">mifizz.pw</a> |
+        <a href="https://t.me/fvckinmifizz">telegram</a> |
+        <a href="https://mastodon.social/@mifizz">mastodon</a> |
+        <a href="https://github.com/mifizz">github</a> |
+        <a href="https://steamcommunity.com/id/mifizz">steam</a>
     </footer>
 `;
--- static/js/header.js
+++ static/js/header.js
@@ -1,6 +1,11 @@
-const headerText = document.getElementById('header-text').textContent;
-document.getElementById('header').innerHTML = `
-    <header>
-        <b>${headerText}</b>
-    </header>
+const headerText = document.getElementById("header-text").textContent;
+document.getElementById("header").innerHTML = `
+  <a href="/">
+  <img class="img-bordered img-head"
+  src="/static/images/head.jpg"
+  alt="Здесь могла быть ваша реклама"/>
+  </a>
+  <header>
+      <b>${headerText}</b>
+  </header>
 `;
